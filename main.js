eb78526c10898a1b97fdda7185e47d307f2ddcc4
PBalazs0517 <peter.balazs0517@gmail.com>
2023-01-02 22:04:00 +0100

    Uploading FEM project, made by following KP's video series

---
+++ main.js
@@ -0,0 +1,20 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+    primaryNav.hasAttribute("data-visible") 
+    ? navToggle.setAttribute("aria-expanded", false) 
+    : navToggle.setAttribute("aria-expanded", true);
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
+});
+
+//slider
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    centerMode: true,
+});
